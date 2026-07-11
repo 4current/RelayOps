63d6da13c5b0e5955922d7fe1b294224f578c135
Richard A Morrisey <r.morrisey@computer.org>
2026-07-11 11:58:11 -0400

    repair env file

--- scripts/common/relayops-env.sh
+++ scripts/common/relayops-env.sh
@@ -17,3 +17,12 @@ relayops_ic9700_card() {
   relayops_card_from_control /dev/snd/by-radio/ic9700-control
 
 }
+
+TS890_CARD="$(relayops_ts890_card)"
+IC9700_CARD="$relayops_ic9700_card)"
+
+export DISPLAY="${DISPLAY:-:0}"
+export XAUTHORITY="${XAUTHORITY:-$HOME/.Xauthority}"
+
+# Common CrossOver settings
+export BOTTLE="Winlink"
